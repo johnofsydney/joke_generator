f782387c5425064319c2bd4490f48a9df30952f0
John Coote <john_coote@rocketmail.com>
2020-11-25 20:28:23 +1100

    2 jokes working

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
 console.log("connected");
 $('#reveal').hide();
+let count = 0;
+
+const jokes = [
+  {
+    question: "A ham sandwich walks into a bar and orders a beer",
+    answer: "the bartender says “sorry, we don’t serve food here."
+  },
+  {
+    question: "how did Darth Vader know what Luke was getting for christmas",
+    answer: "he felt his presents."
+  }
+];
 
 const generateJoke = function () {
-  let question = "A ham sandwich walks into a bar and orders a beer";
+  let question = jokes[count].question;
+  let answer = jokes[count].answer;
 
-  $('#joke-container').append(question);
+  let questionParagraph = `<p id="question-${count}">${question}</p>`;
+  let answerParagraph = `<p id="answer-${count}">${answer}</p>`;
+
+  $('#joke-container').append(questionParagraph);
+  $('#joke-container').append(answerParagraph);
+  $(`#answer-${count}`).hide();
+
+
+  // buttons
   $('#generate').hide();
   $('#reveal').show();
+
+  count ++;
 };
 
-
 const revealAnswer = function () {
-    let answer = "the bartender says “sorry, we don’t serve food here.";
-  
-    
-    $('#joke-container').append(answer);
-    $('#reveal').hide();
-    $('#generate').show();
-  };
+  $(`#answer-${count - 1}`).show();
+
+  // buttons
+  $('#reveal').hide();
+  $('#generate').show();
+};
 
 
 $('#generate').on('click', generateJoke);
